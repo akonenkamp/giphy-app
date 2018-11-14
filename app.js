ad3e71019671cf621ef8f651852079b1cd41a6ed
Konenkamp <n0267144@lm.lmig.com>
2018-11-13 19:36:29 -0800

    nothing quite works, and i think its because my api isn't set up correctly, back to drawing board

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+function searchGiphys(){
 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=&q=taco&limit=5&offset=0&rating=R&lang=en";
 
 // Perfoming an AJAX GET request to our queryURL
@@ -5,3 +6,25 @@ $.ajax({
     url: queryURL,
     method: "GET"
 })
+.then(function(response){
+    console.log(queryURL);
+
+
+// pull info from search form 
+
+
+//assign result of search to html elements and appends to page
+var giphy = $("<img>").attr("src", response.#PLACEHOLDERFORJSON);
+var rating = $("<h3>").text(response.#PLACEHOLDERJSON);
+
+$(".theGiph").empty();
+$(".theGiph").append(giphy, rating);
+});
+}
+
+//search for text entered in box and when button is pressed 
+$(".search").on("click", function(event){
+    event.preventDefault();
+    var giphInput = $(".form-control").val().trim();
+    searchGiphys(giphInput);
+});
